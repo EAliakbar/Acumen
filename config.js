689149fc175a717ca4eee050d0d9c2868fad3af6
Ehsan Aliakbar <e.aliakbar97@gmail.com>
2019-06-11 01:20:48 +0430

    dockerize collector using and running it using pm2

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ let config = {};
 
 config.ACCOUNTING_URL = "http://192.168.88.1/accounting/ip.cgi";
 config.LOCAL_RANGE = ['192.168.88.0/24'];
-config.GRAPHITE_CARBON_URL = 'plaintext://localhost:2003/';
+config.GRAPHITE_CARBON_URL = 'plaintext://graphite:2003/';
 
 module.exports = config;
